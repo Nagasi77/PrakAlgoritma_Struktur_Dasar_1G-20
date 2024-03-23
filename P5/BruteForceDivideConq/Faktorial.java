8d1cc656b27bde6f7b6dcb491013aca9b641b724
myusername. <myemail.>
2024-03-23 11:24:46 +0700

    P6_percobaan1

---
+++ P5/BruteForceDivideConq/Faktorial.java
@@ -0,0 +1,45 @@
+package P5.BruteForceDivideConq;
+
+import java.util.Scanner;
+
+public class Faktorial {
+    public int nilai;
+
+int faktorialBF(int n) {
+    int fakto = 1;
+    for(int i=1; i<=n;i++) {
+        fakto = fakto * i;
+    }
+    return fakto;
+}    
+int faktorialDC(int n){
+    if(n==1) {
+        return 1;
+    }
+    else{
+        int fakto = n * faktorialDC(n-1);
+        return fakto;
+    }
+}
+public static void main(String[] args) {
+    Scanner sc20 = new Scanner(System.in);
+    System.out.println("------------------------------");
+    System.out.println("Masukkan jumlah elemen: ");
+    int iJml = sc20.nextInt();
+
+    Faktorial[] fk = new Faktorial[10];
+    for (int i = 0; i < iJml; i++) {
+        fk[i] = new Faktorial();
+        System.out.println("Masukkan nilai data ke-" +(i+1)+": ");
+        fk[i].nilai = sc20.nextInt();
+    }
+            System.out.println("HASIL - BRUTE FORCE");
+            for (int j = 0; j < iJml; j++) {
+            System.out.println("Hasil penghitungan faktorial menggunakan Brute Force adalah " + fk[j].faktorialBF(fk[j].nilai));            
+            }
+            System.out.println("HASIL - DIVIDE AND CONQUER");
+            for (int k = 0; k < iJml; k++) {
+            System.out.println("Hasil penghitungan faktorial menggunakan Divide and Conquer adalah "+ fk[k].faktorialDC(fk[k].nilai));            
+            }
+}
+}
