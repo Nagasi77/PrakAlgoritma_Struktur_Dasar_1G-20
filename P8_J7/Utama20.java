3aa7cae5ec0fe5d7ba75359eddc3ff592592bd2f
myusername. <myemail.>
2024-04-30 15:02:25 +0700

    P8_J7_Perc1

--- P8_J7/Utama20.java
+++ P8_J7/Utama20.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 
 public class Utama20 {
     public static void main(String[] args) {
-        Gudang20 Gudang = new Gudang20(7);
+        int ukuran = 1;
+        Gudang20 Gudang = new Gudang20(ukuran);
         Scanner sc20 = new Scanner(System.in);
         boolean Nur = true;
         while (Nur == true) {
@@ -12,7 +13,9 @@ public class Utama20 {
             System.out.println("1. Tambah barang");
             System.out.println("2. Ambil barang");
             System.out.println("3. Tampilkan tumpukan barang");
-            System.out.println("4. Keluar");
+            System.out.println("4. Tampilkan barang atas");
+            System.out.println("5. Ubah kapasitas list data");
+            System.out.println("6. Keluar");
             System.out.print("Pilih operasi: ");
             int pilihan = sc20.nextInt();
             sc20.nextLine();
@@ -36,6 +39,13 @@ public class Utama20 {
                     Gudang.tampilkanBarang();
                     break;
                 case 4:
+                    Gudang.lihatBarangTeratas();
+                    break;
+                case 5:
+                    System.out.print("Ubah kapasitas menjadi : ");
+                    ukuran = sc20.nextInt();
+                    break;
+                case 6:
                     Nur = false;
                     break;
                 default:
